18b599b8fef58d7b03acc77e68d7f09ed0cc95e1
wangyu <coder_wangyu@163.com>
2025-05-22 10:47:21 +0800

    alter: header组件改造

--- src/components/HeaderLayout/index.jsx
+++ src/components/HeaderLayout/index.jsx
@@ -1,13 +1,35 @@
-import React, {useState} from 'react';
-import {Layout} from "antd";
+import React from 'react';
 import imgUrl from "../../assets/image/logo.png";
 import "./index.less";
+import {
+	Avatar,
+	Dropdown,
+	Layout
+} from "antd";
 
 const {Header} = Layout;
 
+// 下拉列表配置
+const items = [
+	{
+		key: '1',
+		label: (
+			<a target="_blank" rel="noopener noreferrer">
+				个人中心
+			</a>
+		),
+	},
+	{
+		key: '2',
+		label: (
+			<a target="_blank" rel="noopener noreferrer">
+				退出
+			</a>
+		)
+	}
+]
+
 const HeaderLayout = () => {
-	const [dropDownFlag, setDropDownFlag] = useState(false)
-	
 	return (
 		<Header
 			style={{
@@ -22,21 +44,9 @@ const HeaderLayout = () => {
 			}}
 		>
 			<div style={{flex: '1'}}>通用后台管理系统</div>
-			<div onClick={() => setDropDownFlag(!dropDownFlag)} style={{width: '64px', height: '64px', padding: '7px'}}>
-				<img
-					style={{width: '50px', height: '50px', cursor: 'pointer', clipPath: 'circle(50%)'}}
-					src={imgUrl}
-					alt=""
-				/>
-			</div>
-			{
-				dropDownFlag && <>
-					<div className={'dropDown'}>
-						<div className={'dropDown-item'}>个人中心</div>
-						<div className={'dropDown-item'}>退出登录</div>
-					</div>
-				</>
-			}
+			<Dropdown menu={{items}}>
+				<Avatar src={imgUrl} size={50} gap={0} style={{marginTop: '7px', marginRight: '10px'}}/>
+			</Dropdown>
 		</Header>
 	);
 };
