ce68cf7baf05bc246084894000f92bb1c3175ba9
wangyu <coder_wangyu@163.com>
2025-05-22 17:12:59 +0800

    add: 添加首页用户信息卡片

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Card, Col, Row} from "antd";
+import imgUrl from "../../assets/image/logo.png";
+import "./index.less";
 
 const Home = () => {
 	return (
-		<div>
-			home
+		<div className='home-container'>
+			<Row>
+				<Col span={8}>
+					<Card hoverable>
+						<div className="user-infos">
+							<div className='avator'>
+								<img src={imgUrl} alt=""/>
+							</div>
+							<div className="info">
+								<p style={{fontSize: '40px'}}>Admin</p>
+								<p style={{fontSize: '20px'}}>超级管理员</p>
+							</div>
+						</div>
+						<hr/>
+						<div className="login-infos">
+							<div className="info-item">
+								<p>上次登录时间：</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className="info-item">
+								<p>上次登录地点：</p>
+								<p>内蒙古 呼和浩特市</p>
+							</div>
+						</div>
+					</Card>
+				</Col>
+				<Col span={16}>
+					右侧内容区域
+				</Col>
+			</Row>
 		</div>
 	);
 };
